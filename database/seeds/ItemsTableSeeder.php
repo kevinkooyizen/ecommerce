b9649650cbcc1ec5f068b48a1b60f716cdf41948
Kevin <kevin.koo@beaconsbay.com>
2018-09-29 14:04:13 +0800

    updated item seeding to include categories and brands

--- database/seeds/ItemsTableSeeder.php
+++ database/seeds/ItemsTableSeeder.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Seeder;
 
 use App\Models\Item;
+use App\Models\Category;
 
 class ItemsTableSeeder extends Seeder{
   /**
@@ -12,17 +13,20 @@ class ItemsTableSeeder extends Seeder{
    */
   public function run() {
     $names = ['Dog', 'Cat', 'Donkey', 'Cow', 'Sheep', 'Goat', 'Snake', 'Bear', 'Fish'];
-    $brands = ['Nike', 'Adidas', 'Puma', 'Samsung', 'Apple', 'Tesla', 'Jeep', 'Porsche', 'Mini'];
+    $clothingCategory = Category::where('name', 'clothing')->first();
     foreach ($names as $key => $name) {
       $item = new Item;
       $item->name = $names[$key];
-      $item->brand = $brands[$key];
+      $item->category_id = $clothingCategory->id;
+      $item->brand_id = $key + 1;
       $primaryKey = $key + 1;
       $secondaryKey = $key + 2;
       if ($secondaryKey == 10) $secondaryKey = 1;
       $item->primary_image = "img/product-img/product-{$primaryKey}.jpg";
       $item->secondary_image = "img/product-img/product-{$secondaryKey}.jpg";
+      $item->new = rand(0,1);
       $item->price = $key + 1;
+      $item->discount = rand(0,100);
       $item->save();
     }
   }
