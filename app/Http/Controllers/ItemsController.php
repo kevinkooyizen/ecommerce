70364f1ebda0d795e5ef4160235ecd2471648a26
Koo Yi Zen <kooyizen@hotmail.com>
2018-09-29 14:11:46 +0800

    removed unnecessary login for item controller

--- app/Http/Controllers/ItemsController.php
+++ app/Http/Controllers/ItemsController.php
@@ -8,10 +8,6 @@ use Illuminate\Http\Request;
 
 class ItemsController extends Controller
 {
-  public function __construct(){
-    $this->middleware('auth');
-  }
-
   public function index(Request $request) {
     $items = Item::all();
     
