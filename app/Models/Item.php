5bd312788dc6fc6ef3c77a7e2d94707bdccc23ca
Kevin <kevin.koo@beaconsbay.com>
2018-09-30 19:20:09 +0800

    moved item search into a function in item modal

--- app/Models/Item.php
+++ app/Models/Item.php
@@ -4,7 +4,32 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Item extends Model
-{
-  //
+class Item extends Model {
+
+  public static function searchItem($request) {
+    $items = Item::query();
+    if($request->search != 'all') $items->where('name','LIKE','%'.$request->search.'%');
+    if($request->brand) $items->where('brand_id',$request->brand);
+    if($request->colour) $items->where('colour_id',$request->colour);
+    if($request->category) {
+      $category = Category::find($request->category);
+      $categoryIds = [$request->category];
+      $children = Category::where('parent_id', $request->category)->get();
+      foreach ($children as $child) {
+        $categoryIds[] = $child->id;
+      }
+      $items->whereIn('category_id', $categoryIds);
+    }
+
+    if($request->minPrice) $items->where('price', '>=', $request->minPrice);
+    if($request->maxPrice) $items->where('price', '<=', $request->maxPrice);
+    if($request->sort == "latest") $items->orderBy('new', 'DESC');
+    if($request->sort == "expensive") $items->orderBy('price', 'DESC');
+    if($request->sort == "cheap") $items->orderBy('price', 'ASC');
+
+    $items = $items->paginate(9);
+
+    return $items;
+  }
+  
 }
