5bd2a2c6c0d12273c11e91dd1ed98135527b606c
Koo Yi Zen <kooyizen@hotmail.com>
2018-10-03 23:06:12 +0800

    added orders controller setup

---
+++ app/Http/Controllers/OrdersController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Order;
+
+use Illuminate\Http\Request;
+
+class OrdersController extends Controller {
+
+  public function index(Request $request) {
+
+    return view('shop', compact('items', 'request'));
+  }
+
+}
