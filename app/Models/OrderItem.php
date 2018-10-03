5112b25d7ba84643abc567f1c351f863364cf43d
Koo Yi Zen <kooyizen@hotmail.com>
2018-10-03 22:50:09 +0800

    fixed storing of images

---
+++ app/Models/OrderItem.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+use Auth;
+
+class OrderItem extends Model {
+
+  public function order() {
+    return $this->belongsTo(Order::class);
+  }
+
+}
