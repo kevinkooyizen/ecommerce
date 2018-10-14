a4aa815c18c9157f8f25c51411a94264528e7070
Koo Yi Zen <kooyizen@hotmail.com>
2018-10-14 14:13:11 +0800

    removed unused functions

--- app/Models/Status.php
+++ app/Models/Status.php
@@ -6,18 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Status extends Model {
 
-  public static function newStatus() {
-    return Status::where('name', "New")->first();
-  }
-
-  public static function pendingStatus() {
-    return Status::where('name', "Pending")->first();
-  }
-
-  public static function cancelledStatus() {
-    return Status::where('name', "Cancelled")->first();
-  }
-
   public static function getStatus($statusName) {
     $status = Status::where('name', $statusName)->first();
     if (!$status) {
