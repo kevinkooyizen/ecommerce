59b7e4477506be5dbea4e26d20037937a3a72eac
Koo Yi Zen <kooyizen@hotmail.com>
2018-10-26 21:19:12 +0800

    added wallet menu

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -118,6 +118,7 @@
           <div class="dropdown-menu">
             <a class="dropdown-item line-height-20 text-left w-100" href="/items/owner-shop">My Shop (Traveller)</a>
             <a class="dropdown-item line-height-20 text-left w-100" href="/order-requests">My Requests</a>
+            <a class="dropdown-item line-height-20 text-left w-100" href="#">My Wallet</a>
             <a class="dropdown-item line-height-20 text-left w-100" href="/orders/purchase-requests">Purchase Orders</a>
             <a class="dropdown-item line-height-20 text-left w-100" href="/orders/sale-requests">Sales Orders</a>
             <a class="dropdown-item line-height-20 text-left w-100" href="#" onclick="$('#logoutForm').submit();">Logout</a>
